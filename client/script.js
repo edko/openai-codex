81864b196af7ca6201584f60ecf947196850edba
Edison Ko <edisonko@gmail.com>
2022-12-28 09:40:52 -0800

    update endpoint url

--- client/script.js
+++ client/script.js
@@ -79,14 +79,14 @@ const handleSubmit = async (e) => {
 
     // to focus scroll to the bottom 
     chatContainer.scrollTop = chatContainer.scrollHeight;
-    
+
     // specific message div 
     const messageDiv = document.getElementById(uniqueId)
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5001', {
+    const response = await fetch('https://openai-codex-6d7d.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
